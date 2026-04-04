778fb101cf650c0e45d4d6f6f7931b8506feee15
DeivyAcosta1523-pixel <paulaforero0203@gmail.com>
2026-04-04 12:18:09 -0500

    Remove registrarIngresoEstudiantes function

--- Apropiacion/Ejercicio 2.js
+++ Apropiacion/Ejercicio 2.js
@@ -1,16 +1,1 @@
-// Apropiación/Ejercicio 2.js
-export function registrarIngresoEstudiantes(prompt) {
-  let resultado = "";
-  let contador = 0;
-  while (contador < 5) {
-    let carne = prompt("Estudiante " + (contador + 1) + " - ¿Trae carné? (S/N): ");
-    if (carne.toUpperCase() === "S") {
-      resultado += "Estudiante " + (contador + 1) + " → INGRESA\n";
-    } else {
-      resultado += "Estudiante " + (contador + 1) + " → DENEGADO (sin carné)\n";
-    }
-    contador = contador + 1;
-  }
-  resultado += "Proceso completado.";
-  return resultado;
-}
+
